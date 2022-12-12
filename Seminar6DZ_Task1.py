fd81bd39e02788dbc1c90b1de5e830c192e4a74c
Igor <kugimn@list.ru>
2022-12-12 10:58:57 +0300

    3+

--- Seminar6DZ_Task1.py
+++ Seminar6DZ_Task1.py
@@ -7,9 +7,49 @@
 # список: [], ищем: "123", ответ: -1
 
 
-my_list = ["йцу", "фыв", "ячс", "цук", "йцукен", "йцу"]
+# my_list = ["йцу", "фыв", "ячс", "цук", "йцукен"]
 
-all_occurrences = [index for index, element in enumerate(my_list) if element == 'йцу']
+# all_occurrences = [index for index, element in enumerate(my_list) if element == 'йцу']
+   
+
+# print("The element was found at: " + str(all_occurrences))
+# print(f"The second occurrence of the element was found at: {all_occurrences[1]}" )
+
+
+# # список: ["qwe", "asd", "zxc", "qwe", "ertqwe"], ищем: "qwe", ответ: 3
+# test_list = []
+# print(f"список: {test_list}")
+# test_item = input("Введите искомую строку: ")
+
+
+# def check_list(test_list, test_item):
+#     count = 0
+#     for i in range(len(test_list)):
+#         if test_list[i] == test_item:
+#             count += 1
+#             if count == 2:
+#                 return i
+#     else:
+#         return -1
+
+
+# print(f"ответ: {check_list(test_list, test_item)}")
+
+
+
+# # список: ["qwe", "asd", "zxc", "qwe", "ertqwe"], ищем: "qwe", ответ: 3
+# test_list = ["qwe", "asd", "zxc", "qwe", "ertqwe"]
+# test_item = ('qwe')
+
+# filtering = list(filter(lambda x: test_item in x, test_list))
+# print(filtering)
+
+
+
+my_list = ["qwe", "asd", "zxc", "qwe", "ertqwe"]
+
+all_occurrences = [index for index, element in enumerate(my_list) if element == 'qwe']
+   
 
 print("The element was found at: " + str(all_occurrences))
 print(f"The second occurrence of the element was found at: {all_occurrences[1]}" )
