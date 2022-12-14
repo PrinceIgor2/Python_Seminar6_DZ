1ba106698b58f91cd644279525bf253f83bd0cc3
Igor <kugimn@list.ru>
2022-12-14 17:34:24 +0300

    comm2

--- Task3.py
+++ Task3.py
@@ -1,6 +1,26 @@
 # 3-Сформировать список из N членов последовательности.
 # Для N = 5: 1, -3, 9, -27, 81 и т.д.
 
-user_number = int(input("Введите число: "))
-my_list = [(-3)**i for i in range(user_number)]
-print(f"Итогоый список: {my_list}")
+
+def user_min_max_number(input_string: str, min_num: int = None, max_num: int = None) -> int:
+    """
+    Спрашивает у пользователя число в диапзоне от  min_num до  mах_num:
+    Args:
+    input_string - предложение ввода
+    Returns:
+    int - число
+    """
+    while True:
+        try:
+            num = int(input(input_string))
+            if min_num and num < min_num:
+                print(f'Введите больше {min_num}')
+                continue
+            if max_num and num > max_num:
+                print(f'Введите меньше, чем {max_num}')
+                continue
+            return num
+        except ValueError:
+            print('Похоже это не число, попробуте еще раз')
+
+print(list(map(lambda i: ((-3)**i), [i for i in range(user_min_max_number('Введите количество членов последовательности: '))])))
