8e90a2cbb6174057d8fe76d360123ef784457108
Igor <kugimn@list.ru>
2022-12-14 17:55:43 +0300

    comm4

--- Task4.py
+++ Task4.py
@@ -1,8 +1,6 @@
 # 4 - Дан список URL различных сайтов. Нужно составить список доменных имен сайтов.
 
 
-
-
 domain_list = ['https://dzen.ru/a/Y5cRv6EdHgt6ZBaq/', 
 'https://ladoshki.uecard.ru/schoolAdm/preOrderList/',              
 'https://schools.school.mosreg.ru/reports/default.aspx?school=22853&year=2022&report=statJournal-school&datefrom=14.12.2022&dateto=14.12.2022',
@@ -10,7 +8,6 @@ domain_list = ['https://dzen.ru/a/Y5cRv6EdHgt6ZBaq/',
 'https://ozr-shkkurg.edumsko.ru/activity/innovate'           
 'https://smotrim.ru/pick/serials?utm_source=internal&utm_medium=tabbar-menu&utm_campaign=tabbar-menu',
 'https://e.mail.ru/inbox/?authid=l6zg0f83.8no&back=1%2C1&dwhsplit=s10273.b1ss12743s&from=login&x-login-auth=1&ysclid=l6zg0ejz5e180165402&afterReload=1']
-
 
 
 domain_names = list(map(lambda i: i[ : i.find('/')], [i for i in map(lambda i: i.replace('https://', ''), domain_list)] ))
